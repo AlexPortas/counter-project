3d00d28fa3e557bc143545b67c392cc1335a0bad
Alex Portas <alexalonsoportas@gmail.com>
2022-01-11 13:04:38 +0100

    solucionado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-// Tu código aquí. 
+let contador = document.querySelector("#counter");
+let numeroCont = 0;
+
+document.querySelector(".prevBtn").addEventListener("click", () => {
+    numeroCont--;
+    cambiarSpamCont();
+    cambiarEstilosContador();
+});
+
+document.querySelector(".nextBtn").addEventListener("click", () => {
+    numeroCont++;
+    cambiarSpamCont();
+    cambiarEstilosContador();
+});
+
+function cambiarSpamCont() {
+    contador.textContent = numeroCont;
+}
+
+function cambiarEstilosContador() {
+    if (numeroCont < 0) {
+        contador.style.color = 'red';
+    } else if (numeroCont > 0) {
+        contador.style.color = 'green';
+    } else {
+        contador.style.color = '#333333';
+    }
+}
